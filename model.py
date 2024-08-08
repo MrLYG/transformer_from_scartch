bcf4605981b00e514a2999a67cccc417192e5636
KEN <liyuangang66@gmail.com>
2024-08-08 00:03:58 -0700

    decoder

--- model.py
+++ model.py
@@ -153,3 +153,30 @@ class Encoder(nn.Module):
         for layer in self.layers:
             x = layer(x, mask)
         return self.norms(x)
+
+class DecoderBlock(nn.Module):
+    
+    def __init__(self, self_attention_block: MultiHeadAttention, cross_attention_block: MultiHeadAttention, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.encoder_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+        
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, tgt_mask))
+        x = self.residual_connections[1](x, lambda x: self.encoder_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connections[2](x, self.feed_forward_block)
+        return x
+
+class Decoder(nn.Module):
+        
+        def __init__(self, layers: nn.ModuleList) -> None:
+            super().__init__()
+            self.layers = layers
+            self.norms = LayerNormalization()
+            
+        def forward(self, x, encoder_output, src_mask, tgt_mask):
+            for layer in self.layers:
+                x = layer(x, encoder_output, src_mask, tgt_mask)
+            return self.norms(x)
