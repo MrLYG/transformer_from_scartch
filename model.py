8fb2fffcd7f90a808bb7dfdade5f57ea638231b4
KEN <liyuangang66@gmail.com>
2024-08-08 00:06:04 -0700

    projection

--- model.py
+++ model.py
@@ -180,3 +180,16 @@ class Decoder(nn.Module):
             for layer in self.layers:
                 x = layer(x, encoder_output, src_mask, tgt_mask)
             return self.norms(x)
+        
+class ProjectionLayer(nn.Module):
+    
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+        self.linear = nn.Linear(d_model, vocab_size)
+        
+    def forward(self, x):
+        # (batch_size, seq_len, d_model) -> (batch_size, seq_len, vocab_size)
+        x =  self.linear(x)
+        x = torch.log_softmax(x, dim=-1)
+        
+        return x
